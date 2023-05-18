3003b9886406c67b5ab1dd25268e4191b276431e
KnDellx <15803410520@163.com>
2023-05-19 00:07:06 +0800

    0.0.8

--- src/view/ChessboardComponent.java
+++ src/view/ChessboardComponent.java
@@ -11,6 +11,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.HashSet;
+
 import java.util.Set;
 
 import static model.Constant.CHESSBOARD_COL_SIZE;
